1e64e26384c2a57f9444b410a0aca343d4c8ee34
Taylor O'Brien <toobrien@amazon.com>
2024-07-22 23:05:33 -0700

    .

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ DBN_DT_FMT          = "%Y-%m-%dT%H:%M:%S"
 PERIODS_PER_YEAR    = {
                         "ohlcv-1s": 23 * 60 * 60 * 252
                     }
+PERIOD_NAMES        = {
+                        "ohlcv-1s": "1 second"
+                    }
 N                   = 10_000
 
 
@@ -56,14 +59,14 @@ if __name__ == "__main__":
 
         print(f"\n{symbol}\n")
 
-        print(f"{'pnl (pt):':20}{pnl[-1]:>10.0f}")
-        print(f"{'return:':20}{retc * 100:>10.2f}%")
+        print(f"{'pnl (pt):':30}{pnl[-1]:>10.0f}")
+        print(f"{'return:':30}{retc * 100:>10.2f}%")
 
         print("\nannualized\n")
         
-        print(f"{'return:':20}{mu * ppy * 100:>10.2f}%")
-        print(f"{'stdev:':20}{sigma * sqrt(ppy) * 100:>10.2f}%")
-        print(f"{'sharpe:':20}{sharpe:>10.2f}")
+        print(f"{'return:':30}{mu * ppy * 100:>10.2f}%")
+        print(f"{'stdev:':30}{sigma * sqrt(ppy) * 100:>10.2f}%")
+        print(f"{'sharpe:':30}{sharpe:>10.2f}")
 
         fig = go.Figure()
 
@@ -100,16 +103,20 @@ if __name__ == "__main__":
         fig.add_trace(go.Histogram(x = sampling_dist, name = "sampling distribution (mean return)"))
         fig.add_vline(x = mu, line_color = "#FF00FF")
 
-        returns = sorted(sampling_dist)
-        i       = bisect_left(sampling_dist, mu)
-        p_val   = 1 - i / len(sampling_dist)
+        sampling_dist   = sorted(sampling_dist)
+        i               = bisect_left(sampling_dist, mu)
+        p_val           = 1 - i / len(sampling_dist)
 
         fig.show()
 
-        print(f"{'num samples':20}{N:>10}")
-        print(f"{'sampling mean:':20}{mean(sampling_dist) * 100:>10.2f}%")
-        print(f"{'sampling stdev:':20}{std(sampling_dist) * 100:>10.2f}%")
-        print(f"{f'p(r = {mu * 100:>0.2f}%):':20}{p_val:>10.2f}%")
+        print("\nmonte carlo\n")
+
+        print(f"{'num samples':30}{N:>10}")
+        print(f"{'return period:':30}{PERIOD_NAMES['schema']}")
+        print(f"{'trader mean:':30}{mu * 100:>10.9f}%")
+        print(f"{'sampling mean:':30}{mean(sampling_dist) * 100:>10.9f}%")
+        print(f"{'sampling stdev:':30}{std(sampling_dist) * 100:>10.9f}%")
+        print(f"{f'p(r >= {mu * 100:>0.9f}%):':30}{p_val:>10.2f}")
         print("\n")
 
         print(f"{time() - t0:0.1f}s")
