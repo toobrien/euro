8581d960d89e0c09abe3364aa6a7088353842148
Taylor O'Brien <toobrien@amazon.com>
2024-07-25 22:53:39 -0700

    .

--- generate.py
+++ generate.py
@@ -22,19 +22,41 @@ Config.set_tbl_rows(-1)
 
 if __name__ == "__main__":
 
-    in_fn       = join(".", "csvs", f"{argv[1]}.csv")
-    out_fn      = join(".", "csvs", f"{argv[1][:-3]}_out.csv")
-    parser      = PARSERS[argv[2]]
-    schema      = argv[3]
-    tz          = argv[4]
-    debug       = int(argv[5])
+    in_fn           = join(".", "csvs", f"{argv[1]}.csv")
+    out_fn          = join(".", "csvs", f"{argv[1][:-3]}_out.csv")
+    parser          = PARSERS[argv[2]]
+    schema          = argv[3]
+    tz              = argv[4]
+    debug           = int(argv[5])
+    sym_data, input = parser.parse(in_fn, schema, DBN_DT_FMT, tz)
+    mask            = []
 
-    symbols, input, output = parser.parse(in_fn, schema, tz, DBN_DT_FMT)
+    if input:
 
-    if output:
-
-        output  = sorted(output, key = lambda r: r[1])
         input   = sorted(input, key = lambda r: r[1])
+        output  = []
+
+        for row in input:
+
+            symbol  = row[0] 
+            qty     = row[2]
+            ts      = sym_data[symbol]["ts"]
+            px      = sym_data[symbol]["close"]
+            in_ts   = row[1]
+
+            if in_ts < ts[0] or in_ts > ts[-1]:
+
+                mask.append(0)
+
+                continue
+
+            mask.append(1)
+
+            out_idx = bisect_left(ts, in_ts)
+            out_ts  = ts[out_idx]
+            out_px  = px[out_idx]
+
+            output.append(( symbol, out_ts, out_idx, qty, out_px ))
 
         with open(out_fn, "w") as fd:
 
@@ -46,10 +68,12 @@ if __name__ == "__main__":
 
     if debug:
 
-        for symbol in symbols:
+        input = [ input[i] for i in range(len(input)) if mask[i] ]
+
+        for symbol in sym_data.keys():
 
             in_rows         = [ row for row in input if row[0] == symbol ]
-            in_position     = in_rows[0][3]
+            in_position     = in_rows[0][2]
             in_pnl          = 0.
             in_pnls         = [ in_pnl ]
             
@@ -60,8 +84,8 @@ if __name__ == "__main__":
             
             for i in range(1, len(in_rows)):
 
-                in_pnl          += in_position * (in_rows[i][4] - in_rows[i - 1][4])
-                in_position     += in_rows[i][3]
+                in_pnl          += in_position * (in_rows[i][3] - in_rows[i - 1][3])
+                in_position     += in_rows[i][2]
                 out_pnl         += out_position * (out_rows[i][4] - out_rows[i - 1][4])
                 out_position    += out_rows[i][3]
                 
@@ -70,7 +94,7 @@ if __name__ == "__main__":
 
             in_pnls         = array(in_pnls)
             out_pnls        = array(out_pnls)
-            in_px           = array([ row[4] for row in in_rows ])
+            in_px           = array([ row[3] for row in in_rows ])
             out_px          = array([ row[4] for row in out_rows ])
             diff_px         = out_px - in_px
             diff_pnl        = out_pnls - in_pnls
@@ -81,7 +105,7 @@ if __name__ == "__main__":
                         "symbol":       [ row[0] for row in out_rows ],
                         "in_ts":        [ row[1] for row in in_rows ],
                         "out_ts":       [ row[1] for row in out_rows ],
-                        "in_qty":       [ row[3] for row in in_rows ],
+                        "in_qty":       [ row[2] for row in in_rows ],
                         "out_qty":      [ row[3] for row in out_rows ],
                         "in_px":        in_px,
                         "out_px":       out_px,
--- parsers/tradovate.py
+++ parsers/tradovate.py
@@ -1,11 +1,10 @@
 from    enum                    import  IntEnum
-from    bisect                  import  bisect_left
 from    polars                  import  col, Datetime, read_csv
 from    sys                     import  path
 
 path.append(".")
 
-from    util                    import  adjust_tz, get_sym_data
+from    util                    import  get_sym_data
 
 
 class trade_row(IntEnum):
@@ -25,14 +24,14 @@ class trade_row(IntEnum):
     duration            = 12
 
 
-TV_DT_FMT   = "%m/%d/%Y %H:%M:%S" 
+TV_DT_FMT = "%m/%d/%Y %H:%M:%S" 
 
 
 def parse(
     in_fn:      str,
     schema:     str,
-    tz:         str,
-    out_dt_fmt: str
+    out_dt_fmt: str,
+    tz:         str
 ):
 
     trades      = read_csv(in_fn)
@@ -44,7 +43,6 @@ def parse(
                 )
     symbols     = [ sym[:-2] for sym in list(trades["symbol"].unique()) ]
     input       = []
-    output      = []
     sym_data    = get_sym_data(symbols, schema, out_dt_fmt, tz)
     in_rows     = trades.rows()
 
@@ -56,25 +54,8 @@ def parse(
         in_qty      = trade[trade_row.qty]
         in_buy_px   = trade[trade_row.buyPrice]
         in_sell_px  = trade[trade_row.sellPrice]
-        sym_ts      = sym_data[symbol]["ts"]
-        sym_px      = sym_data[symbol]["open"]
         
-        if in_buy_ts < sym_ts[0] or in_sell_ts > sym_ts[-1]:
-        
-            # no data for trade
+        input.append((symbol, in_buy_ts, in_qty, in_buy_px))
+        input.append((symbol, in_sell_ts, -in_qty, in_sell_px))
 
-            continue
-        
-        out_buy_idx     = bisect_left(sym_ts, in_buy_ts)
-        out_sell_idx    = bisect_left(sym_ts, in_sell_ts)
-        out_buy_ts      = sym_ts[out_buy_idx]
-        out_sell_ts     = sym_ts[out_sell_idx]
-        out_buy_px      = sym_px[out_buy_idx]
-        out_sell_px     = sym_px[out_sell_idx]
-
-        input.append((symbol, in_buy_ts, None, in_qty, in_buy_px))
-        input.append((symbol, in_sell_ts, None, -in_qty, in_sell_px))
-        output.append((symbol, out_buy_ts, out_buy_idx, in_qty, out_buy_px))
-        output.append((symbol, out_sell_ts, out_sell_idx, -in_qty, out_sell_px))
-
-    return symbols, input, output
+    return sym_data, input
